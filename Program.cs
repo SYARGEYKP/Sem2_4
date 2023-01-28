83be3d9c74bfbecb39566f5c570c85528d6a230a
SYARGEY <lss1895@mail.ru>
2023-01-28 13:19:29 +0300

    Семинар 2, задние 4

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 4 * : Напишите программу, которая выводит случайное 
+// трёхзначное число и удаляет вторую цифру этого числа. Не
+//  использовать строки для расчета.
+int RandomInt()
+{
+    Random rnd = new Random();
+    return rnd.Next(100, 1000);
+}
+int number = RandomInt();
+int a = number % 10;
+int b = number / 100;
+int c = b * 10 + a;
+System.Console.WriteLine($"случайное число {number}, новое число {c}");
